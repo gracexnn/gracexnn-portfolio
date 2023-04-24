70dc6a1314a94fb7f05d449fa07fc122c34a6c8f
gracexn <narafaker@gmail.com>
2023-04-25 04:35:05 +0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body>
-        <title>gracexnn</title>
+        <title>gracexnn's Space</title>
         <Main />
         <NextScript />
       </body>
