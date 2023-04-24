0d557c3764dc7121d5b5b8568e1a0e9d5f2d9a73
gracexn <narafaker@gmail.com>
2023-04-25 04:38:56 +0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta property="og:image" content="https://scontent.fuln6-1.fna.fbcdn.net/v/t39.30808-6/275780867_3186133451707633_1009755889215039314_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=s2Th3yAY0hEAX81z118&_nc_ht=scontent.fuln6-1.fna&oh=00_AfBXM0Sl91dy0goRKlnd_ZIuI_gtkRErnkU6R_LgJRoOVw&oe=644B1472" />
+      </Head>
       <body>
         <title>gracexnn's Space</title>
         <Main />
