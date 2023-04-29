58a1842662e1e1fb2b9245e7e68d27f9d58ea503
gracexn <narafaker@gmail.com>
2023-04-30 00:04:32 +0800

    title

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,15 @@ import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero.jsx";
 import Card from "@/components/Card";
 import Footer from "@/components/Footer";
+import Head from "next/head";
 
 export default function App() {
   return (
     <>
+
+    <Head>
+    <title>gracexnn's Space</title>
+    </Head>
       <Navbar />
       <Hero />
       <Card />
