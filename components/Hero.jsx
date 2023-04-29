707ea6ac928953bc3be17c1238a3eb46dd56fd48
gracexn <narafaker@gmail.com>
2023-04-29 23:54:55 +0800

    herro

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -27,7 +27,7 @@ function Hero() {
           <div>
             <img
               class="absolute inset-0 object-cover w-full h-full bg-gray-200 lg:border-l"
-              src="https://scontent.fuln6-1.fna.fbcdn.net/v/t39.30808-6/275780867_3186133451707633_1009755889215039314_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=s2Th3yAY0hEAX81z118&_nc_ht=scontent.fuln6-1.fna&oh=00_AfBXM0Sl91dy0goRKlnd_ZIuI_gtkRErnkU6R_LgJRoOVw&oe=644B1472"
+              src="https://res.cloudinary.com/dp7ejpuvw/image/upload/v1682779820/test/275780867_3186133451707633_1009755889215039314_n_otdrzp.jpg"
               alt=""
             ></img>{" "}
           </div>
