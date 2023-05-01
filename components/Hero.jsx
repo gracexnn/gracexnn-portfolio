73c01829550c84127fd1888767d1dfe79fa38627
Naranmandakh Enkhbat <102289404+gracexnn@users.noreply.github.com>
2023-05-01 21:49:00 +0800

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,12 +1,12 @@
 function Hero() {
   return (
     <main>
-      <div class="flex min-h-screen bg-white ">
-        <div class="flex flex-col justify-center flex-1 px-8 py-8 md:px-12 lg:flex-none lg:px-24">
-          <div class="w-full mx-auto lg:max-w-6xl">
-            <div class="max-w-xl mx-auto text-center lg:p-10 lg:text-left">
+      <div className="flex min-h-screen bg-white ">
+        <div className="flex flex-col justify-center flex-1 px-8 py-8 md:px-12 lg:flex-none lg:px-24">
+          <div className="w-full mx-auto lg:max-w-6xl">
+            <div className="max-w-xl mx-auto text-center lg:p-10 lg:text-left">
               <div>
-                <p class="text-2xl font-medium tracking-tight text-black sm:text-4xl">
+                <p className="text-2xl font-medium tracking-tight text-black sm:text-4xl">
                   Exploring the world of tech
                 </p>
                 <p class="max-w-xl mt-4 text-base tracking-tight text-gray-600">
@@ -23,11 +23,11 @@ function Hero() {
             </div>
           </div>
         </div>
-        <div class="relative flex-1 hidden w-0 lg:block">
+        <div className="relative flex-1 hidden w-0 lg:block">
           <div>
             <img
-              class="absolute inset-0 object-cover w-full h-full bg-gray-200 lg:border-l"
-              src="https://scontent.fuln6-1.fna.fbcdn.net/v/t39.30808-6/275780867_3186133451707633_1009755889215039314_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=s2Th3yAY0hEAX81z118&_nc_ht=scontent.fuln6-1.fna&oh=00_AfBXM0Sl91dy0goRKlnd_ZIuI_gtkRErnkU6R_LgJRoOVw&oe=644B1472"
+              className="absolute inset-0 object-cover w-full h-full bg-gray-200 lg:border-l"
+              src="./public/nar2.jpeg"
               alt=""
             ></img>{" "}
           </div>
